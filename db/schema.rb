42e4df9b25a193e91e7ffb796de496f816eb7c7b
BrunoDuarte <frank.bruno555@gmail.com>
2019-07-04 18:31:10 -0300

    Correcting table, recreating it with initial capital leter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_204751) do
+ActiveRecord::Schema.define(version: 2019_07_04_212919) do
 
   create_table "meetings", force: :cascade do |t|
     t.string "user"
